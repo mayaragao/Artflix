94dbfaa4fcdadceb139765ff6ee5a8530acdead3
Mayara Aragão <mayaraaragao@poli.ufrj.com>
2020-07-28 17:18:14 -0300

    Importando React Router DOM para modelo SPA

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../../assets/img/logo_amarela.png';
 import './Menu.css';
 import Button from '../Button';
@@ -7,11 +8,11 @@ function Menu() {
     return(
         <header>
             <nav className='Menu'>
-                <a href="/"> 
+                <Link to="/"> 
                     <img className="Logo" src={Logo} alt="Artflix Logo" />
-                </a>
+                </Link>
 
-                <Button as='a' className="ButtonLink" href="/">
+                <Button as={Link} className="ButtonLink" to="/cadastro/video">
                     Novo vídeo
                 </Button>
             </nav>
--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/Home';
 
+import { BrowserRouter , Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+
+//criar rapidamente uma pagina de erro
+const Pagina404 = () => (<div>Pagina 404</div>)
 
 ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route component= {Pagina404} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+
+  /* basicamente, o switch vai trocar a rota dos componentes, de acordo com o que
+    foi estabelecido no Route, com o caminho delimitado. Estamos fazendo a troca 
+    de componentes do browser a mao de acordo com os caminhos. */
+
+  /*
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
-);
+*/
+  );
