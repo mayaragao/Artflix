5a4aa90bb9c49af017fc17d0d37200fc7b1d6993
Mayara Aragão <mayaraaragao@poli.ufrj.com>
2020-08-05 17:58:54 -0300

    e utilizando useEffect

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -4,6 +4,7 @@ import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
 
+
 function CadastroCategoria() {
   const valoresIniciais = {
     nome: '',
